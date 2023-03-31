92bfa59a91e38b4bf7d8f8c31c2318feb783b69d
huangliang <huangliang@arkmicro.com>
2023-03-31 13:41:46 +0800

    添加针对仪表的board type ark1668e_devb_dashboard

---
+++ u-boot/board/arkmicro/ark1668e_devb_dashboard/ark1668e_devb_dashboard.c
@@ -0,0 +1,328 @@
+#include <common.h>
+#include <dwmmc.h>
+#include <malloc.h>
+#include <debug_uart.h>
+#include <asm-generic/gpio.h>
+#include <asm/arch/ark-common.h>
+#include <linux/usb/musb.h>
+
+DECLARE_GLOBAL_DATA_PTR;
+
+#define ARK1668_UPDATE_MAGIC	"ada7f0c6-7c86-11e9-8f9e-2a86e4085a59"
+
+#define rSYS_SD_CLK_CFG			*((volatile unsigned int *)(0xe4900058))
+#define rSYS_SD1_CLK_CFG		*((volatile unsigned int *)(0xe490005c))
+#define rSYS_SOFT_RSTNA			*((volatile unsigned int *)(0xe4900074))
+#define rSYS_SOFT_RSTNB			*((volatile unsigned int *)(0xe4900078))
+#define rSYS_DDR_STATUS			*((volatile unsigned int *)(0xe4900180))
+#define rSYS_DDR_IO_CFG			*((volatile unsigned int *)(0xe490019C))
+#define rSYS_PAD_CTRL00			*((volatile unsigned int *)(0xe49001c0))
+#define rSYS_PAD_CTRL01			*((volatile unsigned int *)(0xe49001c4))
+#define rSYS_PAD_CTRL02			*((volatile unsigned int *)(0xe49001c8))
+#define rSYS_PAD_CTRL04			*((volatile unsigned int *)(0xe49001d0))
+#define rSYS_PAD_CTRL05			*((volatile unsigned int *)(0xe49001d4))
+#define rSYS_PAD_CTRL06			*((volatile unsigned int *)(0xe49001d8))
+#define rSYS_PAD_CTRL07			*((volatile unsigned int *)(0xe49001dc))
+#define rSYS_PAD_CTRL08			*((volatile unsigned int *)(0xe49001e0))
+#define rSYS_PAD_CTRL09			*((volatile unsigned int *)(0xe49001e4))
+#define rSYS_PAD_CTRL0A			*((volatile unsigned int *)(0xe49001e8))
+#define rSYS_PAD_CTRL0B			*((volatile unsigned int *)(0xe49001ec))
+#define rSYS_PAD_CTRL0C			*((volatile unsigned int *)(0xe49001f0))
+#define rSYS_PAD_CTRL0D			*((volatile unsigned int *)(0xe49001f4))
+#define rSYS_PAD_CTRL0E			*((volatile unsigned int *)(0xe49001f8))
+#define rSYS_PAD_CTRL38			*((volatile unsigned int *)(0xe49001fc))
+#define rSYS_PAD_CTRL3E			*((volatile unsigned int *)(0xe4900200))
+#define rSYS_PAD_CTRL0F			*((volatile unsigned int *)(0xe4900204))
+#define rSYS_CPU_CTL            *((volatile unsigned int *)(0xe4900208))
+#define rSYS_MFC_GMAC_CTL		*((volatile unsigned int *)(0xe490020c))
+#define rSYS_DEVICE_CLK_CFG7	*((volatile unsigned int *)(0xe4900230))
+#define rSYS_DEVICE_CLK_CFG8	*((volatile unsigned int *)(0xe4900234))
+#define rSYS_USB_CFG			*((volatile unsigned int *)(0xe4900260))
+#define rSYS_USB1_CFG			*((volatile unsigned int *)(0xe490026C))
+
+#define rWDT_CR		*((volatile unsigned int *)(0xe4b00000))
+
+#define MUSB_BASE				0xe0100000
+
+extern const struct musb_platform_ops ark_musb_ops;
+
+static struct musb_hdrc_config musb_config = {
+	.multipoint = 1,
+	.dyn_fifo = 0,
+	.num_eps = 6,
+	.ram_bits = 12
+};
+
+static struct musb_hdrc_platform_data musb_platform_data = {
+	.mode = MUSB_HOST,
+	.config = &musb_config,
+	.power = 100,
+	.platform_ops = &ark_musb_ops,
+};
+
+static void dwmci_select_pad(void)
+{
+	unsigned int val;
+
+	/* use sd/mmc 0 */
+	val = rSYS_PAD_CTRL00;
+	val &= ~((0x7<<18)|(0x7<<15)|(0x7<<12)|(0x7<<9)|(0x7<<6)|(0x7<<3)|(0x7<<0));
+	val |= ((0x1<<18)|(0x1<<15)|(0x1<<12)|(0x1<<9)|(0x1<<6)|(0x1<<3)|(0x1<<0));
+	rSYS_PAD_CTRL00 = val;
+
+	val = rSYS_PAD_CTRL0E;
+	val &= ~((0x7<<27)|(0x7<<24));
+	val |= ((0x1<<27)|(0x1<<24));
+	rSYS_PAD_CTRL0E = val;
+
+	val = rSYS_PAD_CTRL38;
+	val &= ~((0x7<<9)|(0x7<<6)|(0x7<<3)|(0x7<<0));
+	val |= ((0x1<<9)|(0x1<<6)|(0x1<<3)|(0x1<<0));
+	rSYS_PAD_CTRL38 = val;
+
+	/* use sd/mmc 1 */
+    val = rSYS_PAD_CTRL00;
+    val &= ~((0x7<<27)|(0x7<<24)|(0x7<<21));
+    val |= ((0x1<<27)|(0x1<<24)|(0x1<<21));
+    rSYS_PAD_CTRL00 = val;
+
+    val = rSYS_PAD_CTRL01;
+    val &= ~((0x7<<9)|(0x7<<6)|(0x7<<3)|(0x7<<0));
+    val |= ((0x1<<9)|(0x1<<6)|(0x1<<3)|(0x1<<0));
+    rSYS_PAD_CTRL01 = val;
+
+	/* use sdio wifi/mmc 2*/
+    val = rSYS_PAD_CTRL01;
+    val &= ~((0x7<<27)|(0x7<<24)|(0x7<<21)|(0x7<<18)|(0x7<<15)|(0x7<<12));
+    val |= ((0x1<<27)|(0x1<<24)|(0x1<<21)|(0x1<<18)|(0x1<<15)|(0x1<<12));
+    rSYS_PAD_CTRL01 = val;
+
+    val = rSYS_PAD_CTRL02;
+    val &= ~(0x7<<0);
+    val |= (0x1<<0);
+    rSYS_PAD_CTRL02 = val;
+}
+
+/* static void dwmci_reset(void)
+{
+	rSYS_SOFT_RSTNA &= ~((1<<29)|(1<<16));
+	rSYS_SOFT_RSTNB &= ~(1<<3);
+	udelay(100);
+	rSYS_SOFT_RSTNA |= ((1<<29)|(1<<16));
+	rSYS_SOFT_RSTNB |= (1<<3);
+} */
+
+static void usb_controller_reset(void)
+{
+	rSYS_SOFT_RSTNA &= ~(3 << 5);
+	udelay(100);
+	rSYS_SOFT_RSTNA |= 3 << 5;
+	udelay(10);
+	/* enhance usb driving capability */
+	rSYS_USB_CFG = 0x3c2e0020;
+	udelay(10);
+	rSYS_USB1_CFG = 0x3c2e0020;
+}
+
+#define ARK_MMC_CLK     	45000000
+#define ARK_MMC_NUM			3
+struct dwmci_host dwmcihost[ARK_MMC_NUM];
+static int ark_dwmci_init(char *name,u32 regbase, int bus_width, int index)
+{
+    struct dwmci_host *host = NULL;
+    host = &dwmcihost[index];
+	memset(host, 0, sizeof(struct dwmci_host));
+
+	dwmci_select_pad();
+	//dwmci_reset();
+
+	/* config clk in sample delay */
+	//rSYS_SD_CLK_CFG &= ~(0x7f << 13);
+	//rSYS_SD_CLK_CFG |= 52 << 13;
+	//rSYS_SD1_CLK_CFG &= ~(0x7f << 13);
+	//rSYS_SD1_CLK_CFG |= 52 << 13;
+
+	/* mmc clk axipll(720M) / ((7 + 1) * 2) */
+	rSYS_SD_CLK_CFG &= ~0xfff;
+	rSYS_SD_CLK_CFG |= (1 << 8) | (1 << 7) | (1 << 5) | 7;
+	rSYS_SD1_CLK_CFG &= ~0xfff;
+	rSYS_SD1_CLK_CFG |= (1 << 8) | (1 << 7) | (1 << 5) | 7;
+
+    host->name = name;
+    host->ioaddr = (void *)regbase;
+    host->buswidth = bus_width;
+    host->dev_index = index;
+    host->bus_hz = ARK_MMC_CLK;
+    host->fifoth_val = 64;
+	host->fifo_mode = 1;
+
+    add_dwmci(host, host->bus_hz, 400000);
+
+    return 0;
+}
+
+int board_mmc_init(bd_t *bis)
+{
+	ark_dwmci_init("ARK_MMC0", 0xec400000, 8, 0);
+#ifndef CONFIG_SPL_BUILD
+    ark_dwmci_init("ARK_MMC1", 0xec800000, 4, 1);
+#endif
+	ark_dwmci_init("ARK_MMC2", 0xecc00000, 4, 2);
+	return 0;
+}
+
+int dram_init(void)
+{
+	gd->ram_size = get_ram_size((void *)CONFIG_SYS_SDRAM_BASE,
+				    CONFIG_SYS_SDRAM_SIZE);
+
+	return 0;
+}
+
+int board_init(void)
+{
+	unsigned int val;
+
+	/* watchdog disable */
+	rWDT_CR = 0;
+
+	/* cpu1 disable */
+	rSYS_CPU_CTL &= ~(1 << 7);
+
+	/* cpu cnt clk enable */
+	rSYS_CPU_CTL |= 1 << 23;
+
+	/* nand pad enable */
+	val = rSYS_PAD_CTRL08;
+	val &= ~((0x7<<27) | (0x7<<24) | (0x7<<21) | (0x7<<18) | (0x7<<15) | (0x7<<12) | (0x7<<9) | (0x7<<6));
+	val |= (0x1<<27) | (0x1<<24) | (0x1<<21) | (0x1<<18) | (0x1<<15) | (0x1<<12) | (0x1<<9) | (0x1<<6);
+	rSYS_PAD_CTRL08 = val;
+
+	val = rSYS_PAD_CTRL09;
+	val &= ~((0x7<<15) | (0x7<<12) | (0x7<<9) | (0x7<<6) | (0x7<<3) | (0x7<<0));
+	val |= (1<<15) | (1<<12) | (1<<9) | (1<<6) | (1<<3) | (1<<0);//enable nand cle, ale,ren,wen
+	rSYS_PAD_CTRL09 = val;
+
+	/* spi pad enable */
+	val = rSYS_PAD_CTRL09;
+	val &= ~((0x7<<27) | (0x7<<24));
+	val |= (0x2<<27) | (0x2<<24);
+	rSYS_PAD_CTRL09 = val;
+
+	val = rSYS_PAD_CTRL0A;
+	val &= ~((0x7<<3) | (0x7<<0));
+	val |= (0x2<<0);
+	rSYS_PAD_CTRL0A = val;
+
+	/* gmac pad enable */
+	rSYS_PAD_CTRL0C = (1 << 27) | (1 << 24) | (1 << 21) | (1 << 18) | (1 << 15) | (1 << 12) | (1 << 9) |
+						(1 << 6) | (1 << 3) | (1 << 0);
+	//rSYS_PAD_CTRL0D = (1 << 24) | (1 << 9) | (1 << 6) | (1 << 3) | (1 << 0);
+	rSYS_PAD_CTRL0D = (1 << 24) | (1 << 21) | (1 << 18) | (1 << 15) | (1 << 12) | (1 << 9) |
+						(1 << 6) | (1 << 3) | (1 << 0);
+	/* gmac tx clk out */
+	rSYS_PAD_CTRL0F |= (1 << 31);
+
+	/* select rgmii interface */
+	rSYS_MFC_GMAC_CTL &= ~(7 << 1);
+	rSYS_MFC_GMAC_CTL |= (1 << 1);
+
+	/* mac rx clk inv */
+	rSYS_DEVICE_CLK_CFG7 |= (1 << 1);
+	/* mac tx clk inv */
+	rSYS_DEVICE_CLK_CFG8 |= (1 << 7);
+
+	/* i2s0 sadata in */
+	rSYS_PAD_CTRL0F &= ~(1 << 28);
+
+	/* i2s1 sadata out */
+	rSYS_PAD_CTRL0F |= (1 << 29);
+
+	/* select pwm0 pad */
+	rSYS_PAD_CTRL05 &= ~0x7;
+	rSYS_PAD_CTRL05 |= 1;
+
+    return 0;
+}
+
+#ifdef CONFIG_BOARD_EARLY_INIT_F
+int board_early_init_f(void)
+{
+#ifdef CONFIG_DEBUG_UART
+	debug_uart_init();
+#endif
+	return 0;
+}
+#endif
+
+int board_late_init(void)
+{
+	char cmd[128];
+	char *need_update,*update_flash;
+	unsigned int loadaddr;
+	int do_update = 0, update_from_mmc = 1;
+
+	run_command("sf probe", 0);
+
+#ifdef CONFIG_USB_MUSB_HOST
+	usb_controller_reset();
+	musb_register(&musb_platform_data, NULL, (void *)MUSB_BASE);
+#endif
+
+	update_flash = env_get("update_from_flash");
+	need_update = env_get("need_update");
+	printf("###need_update %s,update_flash %s####\n",need_update,update_flash);
+	if (!strcmp(need_update, "yes")) {
+		loadaddr = env_get_hex("loadaddr", 0);
+		if (loadaddr)
+			memset((void*)loadaddr, 0, strlen(ARK1668_UPDATE_MAGIC));
+		sprintf(cmd, "fatload %s %s %s update-magic", "mmc", env_get("sd_dev_part"), env_get("loadaddr"));
+		run_command(cmd, 0);
+		if (loadaddr && !memcmp((void *)loadaddr, ARK1668_UPDATE_MAGIC, strlen(ARK1668_UPDATE_MAGIC))) {
+			do_update = 1;
+			goto update_done;
+		} else {
+			printf("Wrong update magic, do not update from mmc.\n");
+		}
+
+#ifdef CONFIG_USB_MUSB_HOST
+		run_command("usb start", 0);
+		sprintf(cmd, "fatload %s %s %s update-magic", "usb", "0", env_get("loadaddr"));
+		run_command(cmd, 0);
+		if (loadaddr && !memcmp((void *)loadaddr, ARK1668_UPDATE_MAGIC, strlen(ARK1668_UPDATE_MAGIC))) {
+			do_update = 1;
+			update_from_mmc = 0;
+		} else {
+			printf("Wrong update magic, do not update from usb.\n");
+		}
+#endif
+	}
+	else if (!strcmp(update_flash, "yes")){
+
+	    sprintf(cmd, "run updatefromflash");
+		printf("cmd=%s\n", cmd);
+		run_command(cmd, 0);
+
+	}
+
+update_done:
+	if (do_update) {
+		run_command("nand erase.part userdata", 0);
+		env_set("do_update", "yes");
+		if (update_from_mmc) {
+			printf("update form mmc...\n");
+			env_set("update_dev_type", "mmc");
+			env_set("update_dev_part", env_get("sd_dev_part"));
+		} else {
+			printf("update form usb...\n");
+			env_set("update_dev_type", "usb");
+			env_set("update_dev_part", "0");
+		}
+	} else {
+		env_set("do_update", "no");
+	}
+
+	return 0;
+}
+
+
---
+++ u-boot/include/configs/ark1668e_devb_dashboard.h
@@ -0,0 +1,363 @@
+/*
+ * (C) Copyright 2012 Stephen Warren
+ *
+ * See file CREDITS for list of people who contributed to this
+ * project.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * version 2 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#ifndef __CONFIG_H
+#define __CONFIG_H
+
+#include <linux/sizes.h>
+
+#define CONFIG_SYS_NONCACHED_MEMORY		SZ_1M
+
+/* Memory layout */
+#define CONFIG_NR_DRAM_BANKS			1
+#define CONFIG_SYS_SDRAM_BASE		0x40000000
+#define CONFIG_SYS_SDRAM_SIZE		SZ_64M
+#define CONFIG_SYS_MALLOC_LEN		0x400000
+
+#ifdef CONFIG_SPL_BUILD
+#define CONFIG_SYS_INIT_SP_ADDR		0x308000
+#else
+#define CONFIG_SYS_INIT_SP_ADDR \
+	(CONFIG_SYS_SDRAM_BASE + 16 * 1024 - GENERATED_GBL_DATA_SIZE)
+#endif
+
+/* phy auto negotiation timeout */
+#define PHY_ANEG_TIMEOUT    20000
+
+/* SPL */
+#define CONFIG_SPL_TEXT_BASE		0x300000
+#define CONFIG_SPL_MAX_SIZE			0x8000
+
+/* timer base address */
+#define CONFIG_SYS_TIMERBASE		0xe8600000
+
+/* watchdog base address */
+#define CONFIG_WATCHDOG_BASEADDR	0xe4b00000
+
+
+/*
+ * NAND Flash controller Configuration
+ */
+#define CONFIG_SYS_USE_NANDFLASH
+#define CONFIG_SYS_NAND_BASE		0xec000000
+#define CONFIG_SYS_MAX_NAND_DEVICE      1
+#define CONFIG_SYS_NAND_ONFI_DETECTION
+#define CONFIG_SYS_NAND_USE_FLASH_BBT
+#define CONFIG_MTD_PARTITIONS
+#define CONFIG_MTD_DEVICE
+#define NANDARGS \
+	"mtdids=" CONFIG_MTDIDS_DEFAULT "\0" \
+	"mtdparts=" CONFIG_MTDPARTS_DEFAULT "\0" \
+	"bootstrapupdate=if fatload ${update_dev_type} ${update_dev_part} ${loadaddr} ubootspl.bin; " \
+		"then nand erase.part bootstrap; " \
+		"switchecc 1; " \
+		"nand write ${loadaddr} bootstrap ${filesize}; " \
+		"switchecc 0; fi\0" \
+	"fdtupdate=if fatload ${update_dev_type} ${update_dev_part} ${loadaddr} "CONFIG_DEFAULT_FDT_FILE"; " \
+		"then setenv fdtsize ${filesize}; " \
+		"nand erase.part fdt; " \
+		"nand write ${loadaddr} fdt ${filesize}; fi\0" \
+	"kernelupdate=if fatload ${update_dev_type} ${update_dev_part} ${loadaddr} zImage; " \
+		"then setenv kernelsize ${filesize}; " \
+		"nand erase.part kernel; " \
+		"nand write ${loadaddr} kernel ${filesize}; fi\0" \
+	"bootloaderupdate=if fatload ${update_dev_type} ${update_dev_part} ${loadaddr} u-boot.img; " \
+		"then setenv bootloadersize ${filesize}; " \
+		"nand erase.part bootloader; " \
+		"nand write ${loadaddr} bootloader ${filesize}; fi\0" \
+/*	"bootloaderupdate=Updatebootloader ${update_dev_type} ${update_dev_part}; \0" \ 
+*/	"updatefromflash=UpdateFlash 0 0; \0" \
+	"rootfsupdate=if fatload ${update_dev_type} ${update_dev_part} ${loadaddr} rootfs.ubi; " \
+		"then setenv rootfssize ${filesize}; " \
+		"nand erase.part rootfs; " \
+		"nand write ${loadaddr} rootfs ${filesize}; fi\0" \
+	"bootanimationupdate=if fatload ${update_dev_type} ${update_dev_part} ${loadaddr} bootanimation; " \
+		"then setenv bootanimationsize ${filesize}; " \
+		"nand erase.part bootanimation; " \
+		"nand write ${loadaddr} bootanimation ${filesize}; " \
+		"else setenv bootanimationsize 0; fi\0" \
+	"reversingtrackupdate=if fatload ${update_dev_type} ${update_dev_part} ${loadaddr} reversingtrack; " \
+		"then setenv reversingtracksize ${filesize}; " \
+		"nand erase.part reversingtrack; " \
+		"nand write ${loadaddr} reversingtrack ${filesize}; " \
+		"else setenv reversingtracksize 0; fi\0" \
+	"bootloaderupdate_back=if fatload ${update_dev_type} ${update_dev_part} ${loadaddr} u-boot.img; " \
+		"then setenv bootloadersize ${filesize}; " \
+		"nand erase.part bootloader_bak; " \
+		"nand write ${loadaddr} bootloader_bak ${filesize}; fi\0" \
+	"nandargs=setenv bootargs console=ttyS0,115200 " \
+		"earlyprintk  loglevel=3 clk_ignore_unused lpj=2285568 enable_console " \
+		"${mtdparts} " \
+		"root=${nandroot} " \
+		"rootfstype=${nandrootfstype}\0" \
+		"root=/dev/mmcblk0p2 rw rootfstype=ext2 rootwait \0" \
+	"nandroot=ubi0:rootfs rw ubi.mtd=rootfs ubi.fm_autoconvert=1\0" \
+	"nandrootfstype=ubifs rootwait\0" \
+	"nandboot=echo Booting from nand ...; " \
+		"run nandargs; " \
+		"nand read ${fdtaddr} fdt ${fdtsize}; " \
+		"nand read ${kerneladdr} kernel ${kernelsize}; " \
+		"nand read ${bootanimationaddr} bootanimation ${bootanimationsize}; " \
+		"nand read ${reversingtrackaddr} reversingtrack ${reversingtracksize}; " \
+		"bootz ${kerneladdr} - ${fdtaddr}\0" \
+	"sdboot=echo Booting from sd...; " \
+		"run nandargs; " \
+		"fatload mmc ${sd_dev_part} ${fdtaddr} "CONFIG_DEFAULT_FDT_FILE"; " \
+		"fatload mmc ${sd_dev_part} ${kerneladdr} zImage; " \
+		"bootz ${kerneladdr} - ${fdtaddr}\0"
+
+/*
+ * SPI Flash controller Configuration
+ */
+#define CONFIG_SYS_SPI_BASE			0xe4f00000
+#define CONFIG_ENV_SECT_SIZE 		65536
+#define CONFIG_SF_DEFAULT_MODE		(SPI_MODE_0)
+#define CONFIG_ENV_SPI_MODE 		CONFIG_SF_DEFAULT_MODE
+#define CONFIG_SF_DEFAULT_SPEED		2000000
+#define CONFIG_ENV_SPI_MAX_HZ		CONFIG_SF_DEFAULT_SPEED
+
+#define SFARGS \
+	"mtdids=" CONFIG_MTDIDS_DEFAULT "\0" \
+	"mtdparts=" CONFIG_MTDPARTS_DEFAULT "\0" \
+	"bootstrapupdate=fatload ${update_dev_type} ${update_dev_part} ${loadaddr} LOADSYS.BIN; " \
+		"sf erase bootstrap 0; " \
+		"sf write ${loadaddr} bootstrap ${filesize}\0" \
+	"fdtupdate=fatload ${update_dev_type} ${update_dev_part} ${loadaddr} "CONFIG_DEFAULT_FDT_FILE"; " \
+		"setenv fdtsize ${filesize}; " \
+		"sf erase fdt 0; " \
+		"sf write ${loadaddr} fdt ${filesize}\0" \
+	"kernelupdate=fatload ${update_dev_type} ${update_dev_part} ${loadaddr} zImage; " \
+		"setenv kernelsize ${filesize}; " \
+		"sf erase kernel 0; " \
+		"sf write ${loadaddr} kernel ${filesize}\0" \
+	"bootloaderupdate=fatload ${update_dev_type} ${update_dev_part} ${loadaddr} u-boot.bin; " \
+		"setenv bootloadersize ${filesize}; " \
+		"sf erase bootloader 0; " \
+		"sf write ${loadaddr} bootloader ${filesize}\0" \
+	"rootfsupdate=fatload ${update_dev_type} ${update_dev_part} ${loadaddr} rootfs.ubi; " \
+		"setenv rootfssize ${filesize}; " \
+		"sf erase rootfs 0; " \
+		"sf write ${loadaddr} rootfs ${filesize}\0" \
+	"sfargs=setenv bootargs console=ttyS0,115200 " \
+		"earlyprintk  loglevel=3 clk_ignore_unused lpj=2285568 " \
+		"${mtdparts} " \
+		"root=${sfroot} " \
+		"rootfstype=${sfrootfstype}\0" \
+	"sfroot=ubi0:rootfs ro ubi.mtd=rootfs\0" \
+	"sfrootfstype=ubifs rootwait\0" \
+	"sfboot=echo Booting from spi flash ...; " \
+		"run sfargs; " \
+		"sf read ${fdtaddr} fdt ${fdtsize}; " \
+		"sf read ${kerneladdr} kernel ${kernelsize}; " \
+		"bootz ${kerneladdr} - ${fdtaddr}\0" \
+	"sdboot=echo Booting from sd...; " \
+		"run sfargs; " \
+		"fatload mmc ${sd_dev_part} ${fdtaddr} "CONFIG_DEFAULT_FDT_FILE"; " \
+		"fatload mmc ${sd_dev_part} ${kerneladdr} zImage; " \
+		"bootz ${kerneladdr} - ${fdtaddr}\0"
+
+/*
+ * EMMC controller Configuration
+ */
+#define CONFIG_SUPPORT_EMMC_BOOT
+#define CONFIG_SYS_MMC_ENV_DEV	0
+#define EMMCARGS \
+	"emmcparts=blkdevparts=" CONFIG_EMMCPARTS_DEFAULT "\0" \
+	"bootstrapupdate=if fatload ${update_dev_type} ${update_dev_part} ${loadaddr} ubootspl.bin; " \
+		"then emmc erase.part bootstrap; " \
+		"emmc write ${loadaddr} bootstrap ${filesize}; fi\0" \
+	"fdtupdate=if fatload ${update_dev_type} ${update_dev_part} ${loadaddr} "CONFIG_DEFAULT_FDT_FILE"; " \
+		"then setenv fdtsize ${filesize}; " \
+		"emmc erase.part fdt; " \
+		"emmc write ${loadaddr} fdt ${filesize}; fi\0" \
+	"kernelupdate=if fatload ${update_dev_type} ${update_dev_part} ${loadaddr} zImage; " \
+		"then setenv kernelsize ${filesize}; " \
+		"emmc erase.part kernel; " \
+		"emmc write ${loadaddr} kernel ${filesize}; fi\0" \
+	"bootloaderupdate=if fatload ${update_dev_type} ${update_dev_part} ${loadaddr} u-boot.img; " \
+		"then setenv bootloadersize ${filesize}; " \
+		"emmc erase.part bootloader; " \
+		"emmc write ${loadaddr} bootloader ${filesize}; fi\0" \
+/*	"bootloaderupdate=Updatebootloader ${update_dev_type} ${update_dev_part}; \0" \
+*/	"updatefromflash=UpdateFlash 0 0; \0" \
+	"rootfsupdate=if fatload ${update_dev_type} ${update_dev_part} ${loadaddr} rootfs.ubi; " \
+		"then setenv rootfssize ${filesize}; " \
+		"emmc erase.part rootfs; " \
+		"emmc write ${loadaddr} rootfs ${filesize}; fi\0" \
+	"bootanimationupdate=if fatload ${update_dev_type} ${update_dev_part} ${loadaddr} bootanimation; " \
+		"then setenv bootanimationsize ${filesize}; " \
+		"emmc erase.part bootanimation; " \
+		"emmc write ${loadaddr} bootanimation ${filesize}; " \
+		"else setenv bootanimationsize 0; fi\0" \
+	"reversingtrackupdate=if fatload ${update_dev_type} ${update_dev_part} ${loadaddr} reversingtrack; " \
+		"then setenv reversingtracksize ${filesize}; " \
+		"emmc erase.part reversingtrack; " \
+		"emmc write ${loadaddr} reversingtrack ${filesize}; " \
+		"else setenv reversingtracksize 0; fi\0" \
+	"emmcargs=setenv bootargs console=ttyS0,115200 " \
+		"earlyprintk  loglevel=3 clk_ignore_unused lpj=2285568 " \
+		"${emmcparts} " \
+		"root=${emmcroot} " \
+		"rootfstype=${emmcrootfstype}\0" \
+	"emmcroot=/dev/mmcblk0p8 rw\0" \
+	"emmcrootfstype=ext2 rootwait\0" \
+	"emmcboot=echo Booting from emmc ...; " \
+		"run emmcargs; " \
+		"mmc dev ${emmc_dev_part}; " \
+		"emmc read ${fdtaddr} fdt ${fdtsize}; " \
+		"emmc read ${kerneladdr} kernel ${kernelsize}; " \
+		"emmc read ${bootanimationaddr} bootanimation ${bootanimationsize}; " \
+		"emmc read ${reversingtrackaddr} reversingtrack ${reversingtracksize}; " \
+		"bootz ${kerneladdr} - ${fdtaddr}\0"
+
+/* Environment */
+#define CONFIG_ENV_SIZE			8192	//CONFIG_PARTITION_UBOOT_ENV_SIZE
+#define CONFIG_ENV_OFFSET		0x240000
+#define CONFIG_ENV_OFFSET_REDUND	0x2C0000
+
+#define CONFIG_SYS_LOAD_ADDR	(CONFIG_SYS_SDRAM_BASE + 0x1000000)
+
+/* Console UART */
+#define CONFIG_PL011_CLOCK		24000000
+#define CONFIG_PL01x_PORTS      {(void *)CONFIG_SYS_SERIAL0, (void *)CONFIG_SYS_SERIAL1, (void *)CONFIG_SYS_SERIAL2, (void *)CONFIG_SYS_SERIAL3}
+#define CONFIG_BAUDRATE			115200
+#define CONFIG_SYS_SERIAL0      0xe8200000
+#define CONFIG_SYS_SERIAL1      0xe8300000
+#define CONFIG_SYS_SERIAL2      0xe8400000
+#define CONFIG_SYS_SERIAL3      0xe8500000
+
+/* Console configuration */
+#define CONFIG_SYS_CBSIZE		512
+#define CONFIG_SYS_PBSIZE		(CONFIG_SYS_CBSIZE + sizeof(CONFIG_SYS_PROMPT) + 16)
+
+#define CONFIG_BOUNCE_BUFFER
+
+
+#if defined(CONFIG_SD_BOOT)
+
+#define CONFIG_EXTRA_ENV_SETTINGS \
+	"need_update=yes\0" \
+	"update_dev_type=mmc\0" \
+	"update_dev_part=1\0" \
+	"sd_dev_part="CONFIG_SD_DEV_PART"\0" \
+	"emmc_dev_part="CONFIG_EMMC_DEV_PART"\0" \
+	"loadaddr=0x44000000\0" \
+	"fdtaddr=0x42000000\0" \
+	"kerneladdr=0x42100000\0" \
+	"bootanimationaddr=0x5e000000\0" \
+	"bootanimationsize=0\0" \
+	"reversingtrackaddr=0x5ea00000\0" \
+	"reversingtracksize=0\0" \
+	"ipaddr=192.168.5.66\0" \
+	EMMCARGS
+
+#define CONFIG_BOOTCOMMAND	\
+	"if test ${do_update} = yes; then " \
+		"echo display update progess ...; " \
+		"disconfig 0; "\
+		"mtdparts default; " \
+		"echo update ubootspl ...; " \
+		"run bootstrapupdate; " \
+		"disconfig 10; "\
+		"echo update bootloader ...; " \
+		"run bootloaderupdate; " \
+		"disconfig 15; "\
+		"echo update fdt ...; " \
+		"run fdtupdate; " \
+		"disconfig 25; "\
+		"echo update kernel ...; " \
+		"run kernelupdate; " \
+		"disconfig 45; "\
+		"echo update rootfs ...; " \
+		"run rootfsupdate; " \
+		"disconfig 80; "\
+		"echo update bootanimation ...; " \
+		"run bootanimationupdate; " \
+		"disconfig 85; "\
+		"echo update reversingtrack ...; " \
+		"run reversingtrackupdate; " \
+		"disconfig 90; "\
+		"setenv do_update no; " \
+		"disconfig 95; "\
+		"saveenv; " \
+		"disconfig 100; "\
+		"run emmcboot; " \
+	"else " \
+		"run emmcboot; " \
+	"fi"
+
+#elif defined(CONFIG_NAND_BOOT)
+
+#define CONFIG_EXTRA_ENV_SETTINGS \
+	"need_update=yes\0" \
+	"update_dev_type=mmc\0" \
+	"update_dev_part=1\0" \
+	"sd_dev_part="CONFIG_SD_DEV_PART"\0" \
+	"loadaddr=0x44000000\0" \
+	"fdtaddr=0x42000000\0" \
+	"kerneladdr=0x42100000\0" \
+	"bootanimationaddr=0x5e000000\0" \
+	"bootanimationsize=0\0" \
+	"reversingtrackaddr=0x5ea00000\0" \
+	"reversingtracksize=0\0" \
+	"ipaddr=192.168.5.66\0" \
+	"nandfdt="CONFIG_DEFAULT_FDT_FILE"\0" \
+	NANDARGS
+
+#define CONFIG_BOOTCOMMAND	\
+	"if test ${do_update} = yes; then " \
+		"echo display update progess ...; " \
+		"disconfig 0; "\
+		"mtdparts default; " \
+		"echo update ubootspl ...; " \
+		"run bootstrapupdate; " \
+		"disconfig 10; "\
+		"echo update bootloader ...; " \
+		"run bootloaderupdate; " \
+		"disconfig 15; "\
+		"echo update fdt ...; " \
+		"run fdtupdate; " \
+		"disconfig 25; "\
+		"echo update kernel ...; " \
+		"run kernelupdate; " \
+		"disconfig 45; "\
+		"echo update rootfs ...; " \
+		"run rootfsupdate; " \
+		"disconfig 80; "\
+		"echo update bootanimation ...; " \
+		"run bootanimationupdate; " \
+		"disconfig 85; "\
+		"echo update reversingtrack ...; " \
+		"run reversingtrackupdate; " \
+		"disconfig 90; "\
+		"echo update bootloader back ...; " \
+		"run bootloaderupdate_back; " \
+		"disconfig 95; "\
+		"setenv do_update no; " \
+		"echo set need_update no ...; " \
+		"setenv need_update no; " \
+		"disconfig 97; "\
+		"saveenv; " \
+		"disconfig 100; "\
+		"run nandboot; " \
+	"else " \
+		"run nandboot; " \
+	"fi"
+	
+#endif
+
+#endif
+
